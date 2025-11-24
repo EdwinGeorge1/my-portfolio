1819f93af1eb38c97e596fa20e6bb5adf80e7752
Edwin George <tgaedwin@gmail.com>
2025-11-24 20:46:26 +0530

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link, useLocation } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, Link, useLocation } from "react-router-dom";
+
 import { motion } from "framer-motion";
 
 /* ============================================================
